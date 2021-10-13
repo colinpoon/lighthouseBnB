3b9aa5faaa53f155e0fb88847a068ef99ac1efe5
Colin Poon <me@colinpoon.com>
2021-10-13 16:06:53 -0700

    after review

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,53 +1,49 @@
 CREATE DATABASE lightbnb;
 
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+  -- users(id) -----> one to many ------> properties(owner_id)?
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE reservations (
-  id SERIAL PRIMARY KEY,
-  start_date DATE,
-  end_date DATE,
-  property_id INTEGER NOT NULL REFERENCES users(id),
-  -- OR set this as a FOREIGN KEY
-  -- property_id INTEGER NOT NULL REFERENCES properties(id), ** Multiple refrences?  
-  guest_id INTEGER NOT NULL REFERENCES users(id)
-  -- guest_id INTEGER NOT NULL REFERENCES reservation(id)
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
-  id SERIAL PRIMARY KEY,
-  guest_id INTEGER NOT NULL REFERENCES users(id),
-  guest_id SERIAL FORIEGN KEY,
--- one to many? from users(id)?
-
-  property_id INTEGER NOT NULL REFERENCES property(id),
-  reservation_id INTEGER NOT NULL REFERENCES reservations(id),
-  rating SMALLINT,
-  messages TEXT -- does it need NOT NULL ?? cause people can decide not to do this
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  messages TEXT
 );
-CREATE TABLE properties (
-  id SERIAL PRIMARY KEY,
   -- one to one ?
-  owner_id INTEGER NOT NULL REFERENCES users(id),
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL,
-  parking_spaces INTEGER,
-  number_of_bathrooms INTEGER NOT NULL,
-  number_of_bedrooms INTEGER NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_date VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL DEFAULT 0
-  -- do i even bother setting a default value of true or false?
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
-
----> terminal: what to type to step out of a DB (like cd ..)? \c root?
