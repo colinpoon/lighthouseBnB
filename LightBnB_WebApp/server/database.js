09e5cd2f82db27b193780365836d5e6690dc169b
Colin Poon <me@colinpoon.com>
2021-10-14 15:38:26 -0700

    added getUserWithEMail

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,34 +1,57 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 /// Users
 
 /**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+Get a single user from the database given their email.
+@param {String} email The email of the user.
+@return {Promise<{}>} A promise to the user. */
+
+const getUserWithEmail = function (email) {
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      // console.log('EMAIL---|', result.rows[0].email);
+      // console.log('ID---|', result.rows[0].id);
+      if (result.rows[0] && result.rows[0].id) {
+        // console.log('.THEN result.rows[0]---|', result.rows[0]);
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log('ERROR:', err.message);
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = function (id) {
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      if (result.rows[0].id) {
+        console.log('.THEN result.rows[0]---|', result.rows[0]);
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log('ERROR:', err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -38,7 +61,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = function (user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -53,7 +76,7 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
+const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
@@ -66,13 +89,22 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
+const getAllProperties = (options, limit = 10) => {
+  pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      return Object.assign({}, result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   const limitedProperties = {};
   for (let i = 1; i <= limit; i++) {
     limitedProperties[i] = properties[i];
   }
   return Promise.resolve(limitedProperties);
-}
+};
+
 exports.getAllProperties = getAllProperties;
 
 
@@ -81,7 +113,16 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+const addProperty = function (property) {
+  pool
+    .query(`
+  SELECT * 
+  FROM properties 
+  LIMIT`)
+    .then((result) => { })
+    .catch((err) => {
+      console.log(err.message);
+    })
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
