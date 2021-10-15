80c570e93c6a4cb2e551a06056185ec587e9c6a5
Colin Poon <me@colinpoon.com>
2021-10-14 23:08:06 -0700

    ADD property

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -135,25 +135,25 @@ const getAllProperties = function (options, limit = 10) {
   }
   ///////////////////
 
-//   id SERIAL PRIMARY KEY NOT NULL,
-//   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-//   title VARCHAR(255) NOT NULL,
-//   description TEXT,
-//   thumbnail_photo_url VARCHAR(255) NOT NULL,
-//   cover_photo_url VARCHAR(255) NOT NULL,
-//   cost_per_night INTEGER  NOT NULL DEFAULT 0,
-//   parking_spaces INTEGER  NOT NULL DEFAULT 0,
-//   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
-//   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
-//   country VARCHAR(255) NOT NULL,
-//   street VARCHAR(255) NOT NULL,
-//   city VARCHAR(255) NOT NULL,
-//   province VARCHAR(255) NOT NULL,
-//   post_code VARCHAR(255) NOT NULL,
+  //   id SERIAL PRIMARY KEY NOT NULL,
+  //   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  //   title VARCHAR(255) NOT NULL,
+  //   description TEXT,
+  //   thumbnail_photo_url VARCHAR(255) NOT NULL,
+  //   cover_photo_url VARCHAR(255) NOT NULL,
+  //   cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  //   parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  //   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  //   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+  //   country VARCHAR(255) NOT NULL,
+  //   street VARCHAR(255) NOT NULL,
+  //   city VARCHAR(255) NOT NULL,
+  //   province VARCHAR(255) NOT NULL,
+  //   post_code VARCHAR(255) NOT NULL,
 
-//   active BOOLEAN NOT NULL DEFAULT TRUE
+  //   active BOOLEAN NOT NULL DEFAULT TRUE
 
-///////////////////
+  ///////////////////
 
 
   // if an owner_id is passed in, only return properties belonging to that owner.
@@ -225,27 +225,22 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function (property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// }
-// exports.addProperty = addProperty;
-// `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING*`,
-// []
-
-/*(owner_id,
-  title,
-  description,
-  thumbnail_photo_url,
-  cover_photo_url,
-  cost_per_night,
-  parking_spaces,
-  number_of_bathrooms,
-  number_of_bedrooms,
-  country,
-  street,
-  city,
-  province,
-  post_code)*/
+const addProperty = function (property) {
+  //   const propertyId = Object.keys(properties).length + 1;
+  //   property.id = propertyId;
+  //   properties[propertyId] = property;
+  //   return Promise.resolve(property);
+  // }
+  return pool
+  
+  .query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING*`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log('ERROR:', err.message);
+  });
+};
+exports.addProperty = addProperty;
